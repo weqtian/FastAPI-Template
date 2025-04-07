975b9e5014e0708f2058e31ec4148c453e0eb93f
晴天 <weqtian@outlook.com>
2025-04-07 14:26:58 +0800

    :tada: 首次提交

--- app/core/logger.py
+++ app/core/logger.py
@@ -9,7 +9,7 @@
 import sys
 import os.path
 from loguru import logger as loguru_logger
-from app.core.config import get_config, LOG_DIR
+from app.core.config import get_config
 
 
 class Logger:
@@ -19,22 +19,22 @@ class Logger:
         """ 初始化日志 """
         self._logger = loguru_logger
         self._logger.remove()
-        self._setting = get_config()
+        self._config = get_config()
         self._setup_logger()
 
 
     def _setup_logger(self):
         """ 初始化日志 """
         # 添加控制台日志
-        self._logger.add(sys.stderr, format=self._setting.LOG_FORMAT, level=self._setting.LOG_LEVEL)
+        self._logger.add(sys.stderr, format=self._config.LOG_FORMAT, level=self._config.LOG_LEVEL)
         # 添加文件日志
-        if self._setting.LOG_OUTPUT_FILE:
+        if self._config.LOG_OUTPUT_FILE:
             # 按天轮转日志文件
-            app_log_file = os.path.join(LOG_DIR, 'app_{time:YYYY-MM-DD}.log')
+            app_log_file = os.path.join(self._config.LOG_DIR, 'app_{time:YYYY-MM-DD}.log')
             self._logger.add(
                 app_log_file,  # 日志文件路径
-                format=self._setting.LOG_FORMAT,  # 日志格式
-                level=self._setting.LOG_LEVEL,  # 日志级别
+                format=self._config.LOG_FORMAT,  # 日志格式
+                level=self._config.LOG_LEVEL,  # 日志级别
                 rotation="00:00",  # 每天午夜
                 compression="zip",  # 压缩旧文件
                 retention="30 days",  # 保留30天
@@ -43,10 +43,10 @@ class Logger:
             )
 
             # 添加错误日志单独保存
-            error_log_file = os.path.join(LOG_DIR, 'error_{time:YYYY-MM-DD}.log')
+            error_log_file = os.path.join(self._config.LOG_DIR, 'error_{time:YYYY-MM-DD}.log')
             self._logger.add(
                 error_log_file,
-                format=self._setting.LOG_FORMAT,
+                format=self._config.LOG_FORMAT,
                 level="ERROR",  # 仅捕获ERROR及以上级别
                 rotation="00:00",  # 每天午夜
                 compression="zip",  # 压缩旧文件
