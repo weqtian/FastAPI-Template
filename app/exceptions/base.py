6c6142d8f40b7a3322961dcff24fe67f9b636a7a
晴天 <weqtian@outlook.com>
2025-04-07 10:52:39 +0800

    :tada: 首次提交

--- app/exceptions/base.py
+++ app/exceptions/base.py
@@ -13,4 +13,5 @@ class BaseExceptions(Exception):
     def __init__(self, code: int, message: str):
         self.code = code
         self.message = message
+        super().__init__(self.message)
 
