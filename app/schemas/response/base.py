8a3b1b3e07f3f44eff588fb827f9cc93d1426a1f
晴天 <weqtian@outlook.com>
2025-04-09 20:32:56 +0800

    :tada: 首次提交

--- app/schemas/response/base.py
+++ app/schemas/response/base.py
@@ -10,24 +10,12 @@ from pydantic import BaseModel
 from typing import Any, Dict, Optional
 
 
-class Base(BaseModel):
-    """ 基础响应模型 """
-
+class BaseResponse(BaseModel):
+    """
+    基础响应模型
+    """
     code: int = 200
-    message: str = 'success'
+    message: str = "OK"
     data: Optional[Dict[str, Any]] = None
+    errors: Optional[Dict[str, Any]] = None
 
-
-class SuccessResponse(Base):
-    """ 成功响应模型 """
-    code: int = 200
-    message: str = 'success'
-    data: Optional[Dict[str, Any]] = None
-
-
-class FailResponse(Base):
-    """ 失败响应模型 """
-
-    code: int = 500
-    message: str = 'fail'
-    detail: Optional[Dict[str, Any]] = None
