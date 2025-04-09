072af0b5e752c1b0324ebccd4382b186a1c505d3
晴天 <weqtian@outlook.com>
2025-04-09 16:09:26 +0800

    :tada: 首次提交

--- app/exceptions/base.py
+++ app/exceptions/base.py
@@ -9,7 +9,13 @@
 
 
 class BaseExceptions(Exception):
-    """自定义异常基类"""
+    """
+    自定义异常基类
+
+    code：业务状态码
+
+    message：错误信息
+    """
     def __init__(self, code: int, message: str):
         self.code = code
         self.message = message
